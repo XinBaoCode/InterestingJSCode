ea7c40fdbc42eff3e9be49bd57b5271b77aa3ff6
XinBaoCode <1392368311@qq.com>
2022-04-25 10:30:01 +0800

    almost done

--- personalTalkPicture/index.js
+++ personalTalkPicture/index.js
@@ -1,20 +1,3 @@
-// 画出Canvas图像
-function createCanvas() {
-  const canvas = document.getElementById('canvas')
-  const ctx = canvas.getContext('2d')
-
-  const canvasHeight = 300
-  const canvasWidth = 300
-
-  const imgSrc = document.getElementById('upload')
-  console.log(imgSrc)
-  ctx.drawImage(imgSrc, 0, 0, 300, 300)
-}
-
-setInterval(() => {
-  createCanvas()
-}, 1000)
-
 // 展示上传的图片
 function showImg(input) {
   var file = input.files[0]
@@ -22,22 +5,21 @@ function showImg(input) {
   // 图片读取成功回调函数
   reader.onload = function (e) {
     document.getElementById('upload').src = e.target.result
+    document.getElementById('result_src').src = e.target.result
   }
   reader.readAsDataURL(file)
 }
 
-// 将Canvas下载成PNG
-function exportCanvasAsPNG(id, fileName) {
-  var canvasElement = document.getElementById(id)
-  var MIME_TYPE = 'image/png'
-  var imgURL = canvasElement.toDataURL(MIME_TYPE)
-  var dlLink = document.createElement('a')
-  dlLink.download = fileName
-  dlLink.href = imgURL
-  dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(
-    ':'
+//下载海报
+function download() {
+  html2canvas(document.querySelector('.result'), { dpi: 400 }).then(
+    (canvas) => {
+      var img = document.createElement('a')
+      img.href = canvas
+        .toDataURL('image/jpeg')
+        .replace('image/jpeg', 'image/octet-stream')
+      img.download = 'invite.jpg'
+      img.click()
+    }
   )
-  document.body.appendChild(dlLink)
-  dlLink.click()
-  document.body.removeChild(dlLink)
 }
